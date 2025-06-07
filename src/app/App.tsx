7ef79ec628f5ffa96596626d04bdfaff663e3df2
Vladislav <kalachinskii_vl@mail.ru>
2025-06-07 17:42:38 +0300

    test: использование компанента shadcn/ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export function App() {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
-    );
+  return (
+    <>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <Button size={"lg"} variant={"outline"}>
+        Button
+      </Button>
+    </>
+  );
 }
 
 /*
