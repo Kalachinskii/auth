1d8778bc525fee0f52069e610fa64f594b8733c5
Vladislav <kalachinskii_vl@mail.ru>
2025-06-15 11:35:42 +0300

    Создана и мигрирована БД в Prisma

--- src/pages/signup/ui/Signup.tsx
+++ src/pages/signup/ui/Signup.tsx
@@ -4,3 +4,8 @@ import { FormPagesLayoutBox } from "@/shared/ui/layouts/FormPagesLayoutBox";
 export function Signup() {
   return <FormPagesLayoutBox title="Sign up" form={<SignupForm />} />;
 }
+// npm i @prisma/client
+// npx prisma init
+
+// миграция
+// npx prisma migrate dev
