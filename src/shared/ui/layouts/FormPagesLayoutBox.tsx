b1667f3ce40ae3c0f5cea05843d76c749b43eff3
Vladislav <kalachinskii_vl@mail.ru>
2025-06-15 23:23:20 +0300

    Добавлен отступ в  формбокс

--- src/shared/ui/layouts/FormPagesLayoutBox.tsx
+++ src/shared/ui/layouts/FormPagesLayoutBox.tsx
@@ -11,7 +11,7 @@ export function FormPagesLayoutBox({
   return (
     <div className="min-h-screen bg-black flex justify-center items-center">
       <main className="border border-zinc-500 rounded-xl bg-blue-200/10 px-8 pt-8 pb-14 min-w-[300px]">
-        <h1 className="text-4xl text-white">{title}</h1>
+        <h1 className="text-4xl mb-6 text-white">{title}</h1>
         {form}
       </main>
     </div>
