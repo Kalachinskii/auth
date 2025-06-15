bd1b9881950c70874f5561839d01901b8685f592
Vladislav <kalachinskii_vl@mail.ru>
2025-06-15 10:56:03 +0300

    изменения верстки signin / signup

--- src/pages/signup/ui/Signup.tsx
+++ src/pages/signup/ui/Signup.tsx
@@ -1,12 +1,6 @@
 import { SignupForm } from "@/features/auth";
+import { FormPagesLayoutBox } from "@/shared/ui/layouts/FormPagesLayoutBox";
 
 export function Signup() {
-  return (
-    <div className="min-h-screen bg-black flex justify-center items-center">
-      <main className="border border-zinc-500 rounded-xl bg-blue-200/10 px-8 pt-8 pb-14 min-w-[300px]">
-        <h1 className="text-4xl text-white">Sign up</h1>
-        <SignupForm />;
-      </main>
-    </div>
-  );
+  return <FormPagesLayoutBox title="Sign up" form={<SignupForm />} />;
 }
