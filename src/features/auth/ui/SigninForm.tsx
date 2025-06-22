1c52d50936c300d8a4f247a69945934f2e6764b2
Vladislav <kalachinskii_vl@mail.ru>
2025-06-22 23:51:26 +0300

    переписать схемы

--- src/features/auth/ui/SigninForm.tsx
+++ src/features/auth/ui/SigninForm.tsx
@@ -1,9 +1,19 @@
+import { ROUTES } from "@/shared/router/constants";
 import { FormLayout } from "./layouts/FormLayout";
+import useSignin from "../model/useSignin";
+import { SigninFormSchema, type SigninFormData } from "../types";
 
 export const SigninForm = () => {
-  const signinHandler = () => {
-    console.log("signin");
-  };
+    const { signinHandler } = useSignin();
 
-  return <FormLayout buttonTitle="Sign in" onSubmit={signinHandler} />;
+    return (
+        <FormLayout
+            link={{ to: ROUTES.SIGNUP, title: "Sign up" }}
+            buttonTitle="Sign in"
+            onSubmit={signinHandler}
+            route={ROUTES.SIGNIN}
+            // formSchema={SigninFormSchema}
+            // formData={type SigninFormData}
+        />
+    );
 };
--- src/features/auth/ui/SignupForm.tsx
+++ src/features/auth/ui/SignupForm.tsx
@@ -1,16 +1,18 @@
+import { ROUTES } from "@/shared/router/constants";
 import { useSignup } from "../model/useSignup";
 import { FormLayout } from "./layouts/FormLayout";
 
 export const SignupForm = () => {
-  const signupHandler = (data: FormData) => {
-    useSignup();
-  };
+    const signupHandler = (data: FormData) => {
+        useSignup();
+    };
 
-  return (
-    <FormLayout
-      buttonTitle="Sign in"
-      onSubmit={signupHandler}
-      confirmField={true}
-    />
-  );
+    return (
+        <FormLayout
+            link={{ to: ROUTES.SIGNIN, title: "Sign in" }}
+            buttonTitle="Sign in"
+            onSubmit={signupHandler}
+            confirmField={true}
+        />
+    );
 };
