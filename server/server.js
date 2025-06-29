7c8709ae374e84b1b4bf4127c914521cce0c9e55
Vladislav <kalachinskii_vl@mail.ru>
2025-06-29 23:39:08 +0300

    начало работы с server.js

--- server/server.js
+++ server/server.js
@@ -18,86 +18,150 @@ app.use(express.json());
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cors({ origin: "http://localhost:5173" }));
 
+//_________________________________________________
+// проверка формы сервера
+import { z } from "zod";
+
+const formSchemaConst = {
+    emailMin: 6,
+    passwordMin: 4,
+    passwordMax: 20,
+};
+
+const passwordSchema = z
+    .string()
+    .min(
+        formSchemaConst.passwordMin,
+        `Password must not be less than ${formSchemaConst.passwordMin} characters.`
+    )
+    .max(
+        formSchemaConst.passwordMax,
+        `Password must not be more than ${formSchemaConst.passwordMax} characters.`
+    )
+    .regex(/[A-Z]/, "Password must contain capital characters.")
+    .regex(/[a-z]/, "Password must contain small characters.")
+    .regex(/[0-9]/, "Password must contain numeric characters.");
+
+const BaseFormSchema = z.object({
+    email: z
+        .string()
+        .email()
+        .min(
+            formSchemaConst.emailMin,
+            `Email must be at least ${formSchemaConst.emailMin} characters.`
+        ),
+    password: passwordSchema,
+});
+
+export const SigninFormSchema = BaseFormSchema;
+
+export const SignupFormSchema = BaseFormSchema.extend({
+    confirmPassword: passwordSchema.optional(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+});
+//_________________________________________________
+
 // каким образом придёт запрос
 app.get("/", (request, response) => {
-  console.log("успех");
+    console.log("успех");
 
-  response.status(200).json({
-    id: 1,
-    name: "test",
-  });
+    response.status(200).json({
+        id: 1,
+        name: "test",
+    });
 });
 
 // создаьть экземпляр класса
 const prisma = new PrismaClient();
 
 app.post("/signin", (request, response) => {
-  if (!request.body.email || !request.body.password) {
-    return response
-      .status(400)
-      .json({ error: "Вы должны передать email и password" });
-  }
-  const { email, password } = request.body;
-  const users = [
-    { email: "admin@mail.ru", password: "1234" },
-    { email: "user@mail.ru", password: "1234" },
-  ];
-  const user = users.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (user) {
-    // ответ от сервера
-    response.status(200).json({ message: "Вы успешно авторизовались" });
-  } else {
-    response.status(401).json({ error: "Пользователь не найден" });
-  }
+    if (!request.body.email || !request.body.password) {
+        return response
+            .status(400)
+            .json({ error: "Вы должны передать email и password" });
+    }
+
+    const { email, password } = request.body;
+    const users = [
+        { email: "admin@mail.ru", password: "Qwe123" },
+        { email: "user@mail.ru", password: "1234" },
+    ];
+    const user = users.find(
+        (user) => user.email === email && user.password === password
+    );
+    if (user) {
+        // ответ от сервера
+        response.status(200).json({ message: "Вы успешно авторизовались" });
+    } else {
+        response.status(401).json({ error: "Пользователь не найден" });
+    }
 });
 
 app.post("/signup", async (request, response) => {
-  console.log("SIGNUP");
+    // console.log(request.body);
+    /*{
+      email: 'asd@gmail.com',
+      password: 'Qwe123',
+      confirmPassword: 'Qwe123'
+    }*/
 
-  // if (!request.body.email || !request.body.password) {
-  //   return response
-  //     .status(400)
-  //     .json({ error: "Вы должны передать email и password" });
-  // }
-  // const { email, password } = request.body;
+    // проверить
+    const result = SignupFormSchema.safeParse(request.body);
+    if (!result.success) {
+        return response
+            .status(400)
+            .json({ error: result.error.flatten().fieldErrors });
+    }
 
-  const email = "sda@mail.ru";
-  const password = "Asdd12323";
+    const { email, password } = request.body;
+    // await p
+    // return;
+    // ZOD - заменил валидацию
+    // if (!request.body.email || !request.body.password) {
+    //   return response
+    //     .status(400)
+    //     .json({ error: "Вы должны передать email и password" });
+    // }
+    // const { email, password } = request.body;
 
-  // const users = [
-  //   { email: "admin@mail.ru", password: "1234" },
-  //   { email: "user@mail.ru", password: "1234" },
-  // ];
-  // const user = users.some(
-  //   (user) => user.email === email && user.password === password
-  // );
-  // if (user) {
-  //   response.status(400).json({ error: "Пользователь существует" });
-  // } else {
-  //   user.push({ email, password });
-  //   response.status(201).json({ message: "Вы успешно зарегистрировались" });
-  // }
+    // const email = "sda@mail.ru";
+    // const password = "Asdd12323";
 
-  // new user s 0
-  // существует ли пользователь
-  const isUserExist = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-  if (isUserExist) {
-    return response.status(400).json({ error: "Login is already exist" });
-  }
-  //
+    // const users = [
+    //   { email: "admin@mail.ru", password: "1234" },
+    //   { email: "user@mail.ru", password: "1234" },
+    // ];
+    // const user = users.some(
+    //   (user) => user.email === email && user.password === password
+    // );
+    // if (user) {
+    //   response.status(400).json({ error: "Пользователь существует" });
+    // } else {
+    //   user.push({ email, password });
+    //   response.status(201).json({ message: "Вы успешно зарегистрировались" });
+    // }
 
-  await prisma.user.create({
-    data: {
-      email,
-      password,
-    },
-  });
+    // new user s 0
+    // существует ли пользователь
+    const isUserExist = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+    if (isUserExist) {
+        return response.status(400).json({ error: "Login is already exist" });
+    }
+
+    const hashedPassword = await bcript.hash(password, 10);
+
+    await prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+        },
+    });
 });
 
 // будет работать на порте 3000
--- src/features/auth/model/useSignup.ts
+++ src/features/auth/model/useSignup.ts
@@ -1,4 +1,4 @@
-import type { AxiosError } from "axios";
+import { AxiosError } from "axios";
 import type { SignupFormSchema } from "./formSchema";
 import type { z } from "zod";
 import { useNavigate } from "react-router-dom";
@@ -11,11 +11,25 @@ export const useSignup = () => {
 
     const signupHandler = async (data: z.infer<typeof SignupFormSchema>) => {
         try {
-            throw new Error();
+            // throw new Error();
             await authApi.signup(data);
             navigate(ROUTES.HOME);
         } catch (error) {
-            toast.error("Signup fail");
+            if (error instanceof AxiosError) {
+                // преобразовать в массив ошибок
+                // const res = Object.entries(error);
+                //
+                // массив ошибок по нашим полям
+                const errorsArray = Object.entries(error.response?.data.error);
+                // const errorMes = errorsArray.map(
+                //     (err) =>
+                //         `<b>${err[0]}: </b> ${err[1].map(
+                //             (innerErr) => `<span>${innerErr}</span><br>`
+                //         )} <br>`
+                // );
+
+                toast.error("Signup fail");
+            }
         }
     };
 
