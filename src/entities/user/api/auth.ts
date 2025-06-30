319ea47b64093d9900bfb3f1dae6fbe4dfd8a865
Vladislav <kalachinskii_vl@mail.ru>
2025-06-30 20:58:44 +0300

    исправлен интерфейс формы signup / реализовано сохранение токена в куки

--- src/entities/user/api/auth.ts
+++ src/entities/user/api/auth.ts
@@ -1,12 +1,22 @@
 import { api } from "@/shared/api/axios-instance";
 import type { IUser } from "../types";
 import { ROUTES } from "@/shared/api/constants";
+import type { AxiosResponse } from "axios";
 
 interface IUserRequest extends Pick<IUser, "email" | "password"> {}
+interface IUserResponse {
+    id: number;
+    email: string;
+}
+// что из себя представляет ответ от сервера
+interface IUserSignupResponse {
+    token: string;
+    user: IUserResponse;
+}
 
 export const authApi = {
-  signin: (data: IUserRequest) =>
-    api.post<{ message: string }>(ROUTES.SIGNIN, data),
-  signup: (data: IUserRequest) =>
-    api.post<{ message: string }>(ROUTES.SIGNUP, data),
+    signin: (data: IUserRequest) =>
+        api.post<{ message: string }>(ROUTES.SIGNIN, data),
+    signup: (data: IUserRequest) =>
+        api.post<IUserSignupResponse>(ROUTES.SIGNUP, data),
 };
