5d3a14d7f5370cd050232a16ec8aeffb25136a9c
Vladislav <kalachinskii_vl@mail.ru>
2025-07-24 14:28:46 +0300

    fix server protected/ добавлен hoc проверка перехода авторизованного на sign(in/up)

--- src/pages/signin/ui/Signin.tsx
+++ src/pages/signin/ui/Signin.tsx
@@ -1,15 +1,6 @@
-// import { authApi } from "@/entities/user/api/auth";
-import { SigninForm } from "@/features/auth";
+import { SigninForm, withCheckAuth } from "@/features/auth";
 import { FormPagesLayoutBox } from "@/shared/ui/layouts/FormPagesLayoutBox";
-// import { AxiosError } from "axios";
 
-export function Signin() {
-  // authApi
-  //   .signin({ email: "user@mail.ru", password: "1234" })
-  //   .then((resp) => console.log(resp.data.message))
-  //   .catch((error: AxiosError<{ error: string }>) => {
-  //     console.log(error.response?.data.error);
-  //   });
-
+export const Signin = withCheckAuth(() => {
   return <FormPagesLayoutBox title="Sign in" form={<SigninForm />} />;
-}
+});
