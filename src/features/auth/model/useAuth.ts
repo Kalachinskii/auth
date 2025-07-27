2dd81c0dfbf833823782c9aaa563f5498ede1f7f
Vladislav <kalachinskii_vl@mail.ru>
2025-07-27 12:01:29 +0300

    защита токена в куки

--- src/features/auth/model/useAuth.ts
+++ src/features/auth/model/useAuth.ts
@@ -11,33 +11,31 @@ import type { z } from "zod";
 import { authApi } from "@/entities/user/api/auth";
 
 export const useAuth = (ROUTE_VALUE: `${RouteNames}`) => {
-    const [sererValidationErrors, setSererValidationErrors] =
-        useState<ValidationFormFieldTypes | null>(null);
-    const navigate = useNavigate();
-    const authHandler = async (
-        data:
-            | z.infer<typeof SignupFormSchema>
-            | z.infer<typeof SigninFormSchema>
-    ) => {
-        try {
-            const resp = await authApi[ROUTE_VALUE](data);
+  const [sererValidationErrors, setSererValidationErrors] =
+    useState<ValidationFormFieldTypes | null>(null);
+  const navigate = useNavigate();
+  const authHandler = async (
+    data: z.infer<typeof SignupFormSchema> | z.infer<typeof SigninFormSchema>
+  ) => {
+    try {
+      const resp = await authApi[ROUTE_VALUE](data);
 
-            if (!resp.data.token) throw new Error("Нет токена");
+      //   if (!resp.data.token) throw new Error("Нет токена");
 
-            Cookies.default.set("token", resp.data.token, { expires: 1 / 24 });
-            navigate(ROUTES.HOME);
-        } catch (err) {
-            const error = err as AxiosError<{
-                error: string | ValidationFormFieldTypes;
-            }>;
-            if (error.response?.data.error instanceof Object) {
-                setSererValidationErrors(error.response?.data.error);
-            } else {
-                // модалкой выводим текстовые ошибки
-                toast.error(error.response?.data.error);
-            }
-        }
-    };
+      // Cookies.default.set("token", resp.data.token, { expires: 1 / 24 });
+      navigate(ROUTES.HOME);
+    } catch (err) {
+      const error = err as AxiosError<{
+        error: string | ValidationFormFieldTypes;
+      }>;
+      if (error.response?.data.error instanceof Object) {
+        setSererValidationErrors(error.response?.data.error);
+      } else {
+        // модалкой выводим текстовые ошибки
+        toast.error(error.response?.data.error);
+      }
+    }
+  };
 
-    return { authHandler, sererValidationErrors };
+  return { authHandler, sererValidationErrors };
 };
