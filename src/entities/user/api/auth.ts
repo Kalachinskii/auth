1b8c1adf78952127b9feecf4914f91a7388b4067
Vladislav <kalachinskii_vl@mail.ru>
2025-07-27 11:13:20 +0300

    fix: protected api

--- src/entities/user/api/auth.ts
+++ src/entities/user/api/auth.ts
@@ -1,29 +1,28 @@
-import { api } from "@/shared/api/axios-instance";
+import { api, protectedApi } from "@/shared/api/axios-instance";
 import type { IUser } from "../types";
 import { ROUTES } from "@/shared/api/constants";
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
 interface IUserRequest extends Pick<IUser, "email" | "password"> {}
 interface IUserData {
-    id: number;
-    email: string;
+  id: number;
+  email: string;
 }
 // что из себя представляет ответ от сервера
 interface IUserResponse {
-    token: string;
-    user: IUserData;
+  token: string;
+  user: IUserData;
 }
 
 export const authApi = {
-    signin: (data: IUserRequest) =>
-        api.post<IUserResponse>(ROUTES.SIGNIN, data),
-    signup: (data: IUserRequest) =>
-        api.post<IUserResponse>(ROUTES.SIGNUP, data),
-    protected: () =>
-        api.get(ROUTES.PROTECTED, {
-            headers: {
-                // описываем токен
-                Authorization: `Bearer ${Cookies.get("token")}`,
-            },
-        }),
+  signin: (data: IUserRequest) => api.post<IUserResponse>(ROUTES.SIGNIN, data),
+  signup: (data: IUserRequest) => api.post<IUserResponse>(ROUTES.SIGNUP, data),
+  // protected: () =>
+  //     api.get(ROUTES.PROTECTED, {
+  //         headers: {
+  //             // описываем токен
+  //             Authorization: `Bearer ${Cookies.get("token")}`,
+  //         },
+  //     }),
+  protected: () => protectedApi.get(ROUTES.PROTECTED),
 };
