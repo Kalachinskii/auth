82b558176b41d10459caa27d901857dec8e6f1ee
Vladislav <kalachinskii_vl@mail.ru>
2025-08-03 23:22:39 +0300

    fix: вынесена логика home в модель

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,16 +1,7 @@
-// npm i zustand
-import { useUserStore } from "@/entities/user";
-import type { IUserPublic } from "@/entities/user/types";
-import { useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useUser } from "../model/useUser";
 
 export function Home() {
-  const { user } = useLoaderData<{ user: IUserPublic }>();
-  const setUser = useUserStore((state) => state.setUser);
-
-  useEffect(() => {
-    if (user) setUser(user);
-  }, [user]);
+  useUser();
 
   return <div>Home</div>;
 }
